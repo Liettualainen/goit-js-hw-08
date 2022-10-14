463b8ebb9245f1cfa67aa49b97464677b6ad46b9
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2022-10-14 15:58:19 +0300

    1strevision

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle( evt => {
+    localStorage.setItem('videoplayer-current-time', evt.seconds);
+}, 1000)
+);
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0)
+.catch(function (error) {
+    console.error(error);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const feedbackform = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+feedbackform.addEventListener('input', throttle(evt => {
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+  }, 500)
+);
+
+feedbackform.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  feedbackform.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const safeTheData = load(LOCALSTORAGE_KEY);
+if (safeTheData) {
+  email.value = safeTheData.email;
+  message.value = safeTheData.message;
+}
